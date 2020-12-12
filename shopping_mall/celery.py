ce61e3ae5606b79812742dd0b418f94b7d6acc50
Akinade Mayowa Emmnauel <akinade.mayowa@gmail.com>
2020-12-12 02:32:12 +0100

    Delete celery.py

--- shopping_mall/celery.py
+++
@@ -1,7 +0,0 @@
-import os
-from celery import Celery
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopping_mall.settings')
-app = Celery('shopping_mall')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
